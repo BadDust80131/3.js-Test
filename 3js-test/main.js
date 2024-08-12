075fafb3e7d99a9933312435dcee8f4d7b7ee16d
BadDust80131 <45376544+BadDust80131@users.noreply.github.com>
2024-08-11 23:11:16 -0700

    Organized and commented code

--- 3js-test/main.js
+++ 3js-test/main.js
@@ -1,58 +1,42 @@
+// Required Imports
 import './style.css'
-
 import * as THREE from 'three';
-
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 import * as gsap from "gsap";
 
+// Create base objects needed for rendering
 const scene = new THREE.Scene();
-
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector("#bg"),
-
 })
 
+// Configure Renderer and render blanks screen
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
-
 renderer.render(scene, camera);
 
-
+// Generates torus with material and adds to scene
 const geometry = new THREE.TorusGeometry(10,0.1,16,100);
 const material = new THREE.MeshStandardMaterial({color: 0x000000});
 const torus = new THREE.Mesh(geometry,material);
-
 scene.add(torus);
 
+// Creates required lighting
 const worldLight = new THREE.AmbientLight(0xffffff);
 const pointLight = new THREE.PointLight(0xffffff, 10000, 100);
 pointLight.position.set(20,20,20);
-
 scene.add(pointLight)
 scene.add(worldLight)
 
-
+// Object needed for orbital controls
 const controls = new OrbitControls(camera, renderer.domElement);
 
-
-function addStar() {
-  const geometry = new THREE.SphereGeometry(0.25,24,24);
-  const material = new THREE.MeshStandardMaterial({color: 0xffffff});
-  const star = new THREE.Mesh(geometry,material);
-
-  const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(200));
-
-  star.position.set(x,y,z);
-  scene.add(star);
-}
-
+// Calls star function to fill area with spheres
 Array(500).fill().forEach(addStar)
 
-
-
+// Loads a cube texture and applies it to the background.
 const loader = new THREE.CubeTextureLoader();
 const texture = loader.load([
   "public/xpos.png",
@@ -64,10 +48,9 @@ const texture = loader.load([
 ]);
 scene.background = texture;
 
-
+// Creates a moon with appropriate textures then adds to scene
 const moonTexture = new THREE.TextureLoader().load("public/moon.jpg");
 const moonNormalTexture = new THREE.TextureLoader().load("public/normal.jpg");
-
 const moon = new THREE.Mesh(
   new THREE.SphereGeometry(3,32,32),
   new THREE.MeshStandardMaterial({
@@ -75,25 +58,35 @@ const moon = new THREE.Mesh(
     normalMap: moonNormalTexture,
   })
 );
-
 scene.add(moon);
 
+// Function used to create a star that is randomly placed. Called at the start of the script. 
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25,24,24);
+  const material = new THREE.MeshStandardMaterial({color: 0xffffff});
+  const star = new THREE.Mesh(geometry,material);
 
+  const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(200));
+
+  star.position.set(x,y,z);
+  scene.add(star);
+}
+
+// Main Loop, used for animating and updating controls.
 function animate() {
   requestAnimationFrame(animate);
-  
 
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
 
   controls.update();
-
   renderer.render(scene,camera);
 }
 
-
-function exploreAnimation() {
+// Centers the camera to the moon object. Animates the whole thing. 
+function centerAnimation() {
+  // Rotates camera towards moon.
   gsap.gsap.to(controls.target, {
     x: moon.position.x,
     y: moon.position.y,
@@ -101,6 +94,7 @@ function exploreAnimation() {
     duration: 2,
     ease: "power3.inOut",
   })
+  // Moves camera towards moon, offset by 30.
   gsap.gsap.to(camera.position, {
     x: 0,
     y: 0,
@@ -110,13 +104,14 @@ function exploreAnimation() {
   }, "-=2");
 }
 
+// Creates a listener then calles centerAnimation when key is pressed.
 addEventListener("keydown", onkeydown);
-
 function onkeydown(event){
   if(event.key === "e" || event.key === "E") {
     console.log("e")
-    exploreAnimation()
+    centerAnimation()
   }
 }
 
+// Calls the main loop
 animate()
